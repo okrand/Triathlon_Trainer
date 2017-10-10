5adffafb0ffecefad3766ae8588bd82c7f688947
okrand <orkun.krand@gmail.com>
2017-10-10 17:38:57 -0400

    Just give it a go

--- Triathlon Trainer WatchKit Extension/InterfaceController.swift
+++ Triathlon Trainer WatchKit Extension/InterfaceController.swift
@@ -10,8 +10,8 @@ import WatchKit
 import CoreMotion
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
+    
     let motion = CMMotionManager()
     
     func startAccelerometers() {
@@ -19,9 +19,26 @@ class InterfaceController: WKInterfaceController {
         if self.motion.isAccelerometerAvailable {
             self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
             self.motion.startAccelerometerUpdates()
+            
+            // Configure a timer to fetch the data.
+            let timer = Timer(fire: Date(), interval: (1.0/60.0),
+                               repeats: true, block: { (timer) in
+                                // Get the accelerometer data.
+                                if let data = self.motion.accelerometerData {
+                                    let x = data.acceleration.x
+                                    let y = data.acceleration.y
+                                    let z = data.acceleration.z
+                                    
+                                    // Use the accelerometer data in your app.
+                                    var XAxis: WKInterfaceLabel!
+                                    XAxis.setText(String(x))
+                                }
+            })
+            
+            // Add the timer to the current run loop.
+            RunLoop.current.add(timer, forMode: .defaultRunLoopMode)
         }
     }
-    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
--- Triathlon TrainerTests/Triathlon_TrainerTests.swift
+++ Triathlon TrainerTests/Triathlon_TrainerTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class Triathlon_TrainerTests: XCTestCase {
     
+    
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
