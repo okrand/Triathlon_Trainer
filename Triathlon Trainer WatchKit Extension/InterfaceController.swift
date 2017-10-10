1c764670086c2e68ffeff5604437da7fbe8ae321
okrand <orkun.krand@gmail.com>
2017-10-10 16:33:04 -0400

    Did things, got our first UI

--- Triathlon Trainer WatchKit Extension/InterfaceController.swift
+++ Triathlon Trainer WatchKit Extension/InterfaceController.swift
@@ -7,11 +7,22 @@
 //
 
 import WatchKit
+import CoreMotion
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    let motion = CMMotionManager()
+    
+    func startAccelerometers() {
+        // Make sure the accelerometer hardware is available.
+        if self.motion.isAccelerometerAvailable {
+            self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+            self.motion.startAccelerometerUpdates()
+        }
+    }
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
