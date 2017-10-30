c6ad0f33c21129cc3e73a7d6d6c5f92158104ced
Justin <jscha002@odu.edu>
2017-10-30 09:00:53 -0400

    simplified a bit

--- Triathlon Trainer WatchKit Extension/InterfaceController.swift
+++ Triathlon Trainer WatchKit Extension/InterfaceController.swift
@@ -72,36 +72,36 @@ class InterfaceController: WKInterfaceController {
             updateButtonText(newText: "Not Ready!!")
         }
         // Make sure the accelerometer hardware is available.
-        if self.motion.isAccelerometerAvailable {
-            self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
-            self.motion.startAccelerometerUpdates()
-        }
-        if self.motion.isGyroAvailable{
-            self.motion.gyroUpdateInterval = 1.0 / 60.0 //60 Hz
-            self.motion.startGyroUpdates()
-        }
-        if self.motion.isMagnetometerAvailable{
-            self.motion.magnetometerUpdateInterval = 1.0 / 60.0 //60 Hz
-            self.motion.startMagnetometerUpdates()
-        }
+        //if self.motion.isAccelerometerAvailable {
+       //     self.motion.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
+       //     self.motion.startAccelerometerUpdates()
+      //  }
+      //  if self.motion.isGyroAvailable{
+      //      self.motion.gyroUpdateInterval = 1.0 / 60.0 //60 Hz
+      //      self.motion.startGyroUpdates()
+      //  }
+     //   if self.motion.isMagnetometerAvailable{
+      //      self.motion.magnetometerUpdateInterval = 1.0 / 60.0 //60 Hz
+      //      self.motion.startMagnetometerUpdates()
+      //  }
         if self.motion.isDeviceMotionAvailable{
             self.motion.deviceMotionUpdateInterval = 1.0 / 60.0 // 60 Hz
             self.motion.startDeviceMotionUpdates()
         }
             // Configure a timer to fetch the data.
-            timer = Timer(fire: Date(), interval: (1.0/60.0),// 60 hz
+            timer = Timer(fire: Date(), interval: (1.0/60.0),
                                repeats: true, block: { (timer) in
                                 // Get the accelerometer data.
-                                if let adata = self.motion.accelerometerData {
-                                    let x = String(format: "%.4f", adata.acceleration.x)
-                                    let y = String(format: "%.4f", adata.acceleration.y)
-                                    let z = String(format: "%.4f", adata.acceleration.z)
+                                //if let adata = self.motion.accelerometerData {
+                                   // let x = String(format: "%.4f", adata.acceleration.x)
+                                  //  let y = String(format: "%.4f", adata.acceleration.y)
+                                  //  let z = String(format: "%.4f", adata.acceleration.z)
                                     
                                  //Use the accelerometer data in your app.
-                                    self.XAxis.setText("X: "+x)
-                                    self.YAxis.setText("Y: "+y)
-                                    self.ZAxis.setText("Z: "+z)
-                                }
+                                  //  self.XAxis.setText("X: "+x)
+                                  //  self.YAxis.setText("Y: "+y)
+                                  //  self.ZAxis.setText("Z: "+z)
+                                //}
                                 //if let rdata = self.motion.gyroData {
                                     //let rx = String(format: "%.4f", rdata.rotationRate.x)
                                     //let ry = String(format: "%.4f", rdata.rotationRate.y)
@@ -111,15 +111,15 @@ class InterfaceController: WKInterfaceController {
                                     //self.YRotAxis.setText("Y: "+ry)
                                     //self.ZRotAxis.setText("Z: "+rz)
                                 //}
-                                if let mdata = self.motion.magnetometerData {
-                                    let mx = String(format: "%.4f", mdata.magneticField.x)
-                                    let my = String(format: "%.4f", mdata.magneticField.y)
-                                    let mz = String(format: "%.4f", mdata.magneticField.z)
+                               // if let mdata = self.motion.magnetometerData {
+                                //    let mx = String(format: "%.4f", mdata.magneticField.x)
+                                //    let my = String(format: "%.4f", mdata.magneticField.y)
+                                //    let mz = String(format: "%.4f", mdata.magneticField.z)
                                     
-                                    self.XMagAxis.setText("X: "+mx)
-                                    self.YMagAxis.setText("Y: "+my)
-                                    self.ZMagAxis.setText("Z: "+mz)
-                                }
+                                //    self.XMagAxis.setText("X: "+mx)
+                                //    self.YMagAxis.setText("Y: "+my)
+                                //    self.ZMagAxis.setText("Z: "+mz)
+                               // }
                                 if let odata = self.motion.deviceMotion{
                                     let Ax = String(format: "%.4f", odata.userAcceleration.x)
                                     let Ay = String(format: "%.4f", odata.userAcceleration.y)
