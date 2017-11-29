d1f79ea4437a4d27320a08e872a0a95589fb0c64
Justin <jscha002@odu.edu>
2017-11-29 16:21:26 -0500

    Plotting

--- Python_Files/Tri_Analysis.py
+++ Python_Files/Tri_Analysis.py
@@ -3,51 +3,68 @@ import pandas
 from sys import argv
 import csv
 from sklearn import svm
+import re
 
 Activity_file=argv[1]
 A_list=[]
 P_list=[]
 stamps=[]
-
-with open(Activity_file, 'r') as infile:
-    read = csv.reader(infile)
-    A_list = list(read)
-
-A_list = sorted(A_list, key=lambda x: x[0])
-
-print('processing file')
-
-P_list = list(Process_Features(A_list, 30))
-
-filename='processed_'.rstrip() + Activity_file
-
-with open(filename, 'w') as outfile:
-    write = csv.writer(outfile)
-    for item in P_list:
-        write.writerow(item)
-for row in P_list:
-    stamps.append(row.pop(0))
-    
-print("stamps: ")
-print(len(stamps))
-print("P_list: ")
-print(len(P_list))
-
-
-path = './training_data'
-actual, t_list = getTrainingData(path)
-
-model = svm.SVC(kernel='linear', C=1, gamma=10).fit(t_list, actual)
-
-Pc_list=model.predict(P_list)
-
-R_list=list(zip(Pc_list,stamps))
-
+s_list=[]
+##p=re.compile(r'\d+\.\d+')
+##
+##with open(Activity_file, 'r') as infile:
+##    read = csv.reader(infile)
+##    A_list = list(read)
+##
+##A_list = sorted(A_list, key=lambda x: x[0])
+##
+##print('processing file')
+##
+##P_list = list(Process_Features(A_list, 30))
+##
+##filename='processed_'.rstrip() + Activity_file
+##
+##with open(filename, 'w') as outfile:
+##    write = csv.writer(outfile)
+##    for item in P_list:
+##        write.writerow(item)
+##for row in P_list:
+##    stamps.append([row.pop(0)])
+##
+##for item in stamps:
+##    s_list.append([float(i) for i in p.findall(str(item))])
+##
+##stampa=[i[0] for i in s_list]
+##stampb=[i[1] for i in s_list]
+##    
+##print("stamps: ")
+##print(len(stamps))
+##print("P_list: ")
+##print(len(P_list))
+##
+##
+##path = './training_data'
+##actual, t_list = getTrainingData(path)
+##
+##model = svm.SVC(kernel='linear', C=1, gamma=10).fit(t_list, actual)
+##
+##Pc_list=model.predict(P_list)
+##
+##R_list=list(zip(Pc_list,stampa, stampb))
+##
 filename='results_'.rstrip() +Activity_file
+##
+##with open(filename, 'w') as outfile:
+##    write = csv.writer(outfile)
+##    for item in R_list:
+##        write.writerow(item)
 
-with open(filename, 'w') as outfile:
-    write = csv.writer(outfile)
-    for item in R_list:
-        write.writerow(item)
+with open(filename, 'r') as infile:
+    read = csv.reader(infile)
+    R_list=list(read)
+
+E_list = Process_Activity(R_list, Activity_file)
+
+plotResults(E_list, R_list)
 
 
--- Python_Files/Tri_Functions.py
+++ Python_Files/Tri_Functions.py
@@ -1,13 +1,20 @@
 import sklearn
 import pandas as pd
-#import numpy as np
+import numpy as np
 import statistics as stat
 import peakutils
 import os
 import csv
 import codecs
+import matplotlib.pyplot as plt
 
-
+def plotResults(E_list, R_list):
+    start=np.array([float(i[1]) for i in E_list])
+    stop=np.array([float(i[2]) for i in E_list])
+    activity=np.array([i[0] for i in E_list])
+    plt.barh(range(len(start)), stop-start, left=start, color=['red','blue'])
+    plt.yticks(range(len(activity)), activity)
+    plt.show()
 
 def Process_Features(l, n):
     Rheaders=['time','Ax','Ay','Az','Gx','Gy','Gz']
@@ -125,6 +132,71 @@ def getTrainingData(path):
             write.writerow(item)       
 
     return Actual, T_list
-            
-        
 
+def Process_Activity(R_list, Activity_file):
+    firstStart=0
+    firstStop=0
+    secondStart=0
+    secondStop=0
+    #thirdStart=0
+    #thirdStop=0
+    first=''
+    second=''
+    #third=''
+    activities=['run','bike']## add 'swim'
+    RR_list=R_list[::-1]
+    Ccounter=0 #current counter
+    Tcounter=0
+    for row in R_list:
+        if first == '':
+            if row[0] in activities:
+                first=row[0]
+                firstStart=row[1]
+                print(first, firstStart)
+        elif second == '':
+            if row[0] == first:
+                if Tcounter<3:
+                    firstStop=row[2]
+                if Ccounter>2:
+                    firstStop=row[2]
+                    Tcounter=0                   
+                Ccounter= Ccounter+1
+            elif row[0] in activities:
+                second=row[0]
+                secondStart=row[1]
+                print(first, firstStop)
+                print(second, secondStart)
+            else:
+                Ccounter=0
+                Tcounter= Tcounter+1
+        else: ##elif third == '':
+            if row[0] == second:
+                if Tcounter<3:
+                    secondStop=row[2]
+                if Ccounter>2:
+                    secondStop=row[2]
+                    Tcounter=0                   
+                Ccounter= Ccounter+1
+##            elif row[0] in activities:
+##                third=row[0]
+##                thirdStart=row[1]
+##                print(second, secondStop)                               
+##                print(third, thirdStart)
+            elif row[0] not in activities: ##else:
+                Ccounter=0
+                Tcounter= Tcounter+1               
+                
+##        else: 
+##            if row[0] == third:
+##                if Ccounter>0 and Tcounter<3:
+##                    thirdStop=row[2]
+##                if Ccounter>3:
+##                    thirdStop=row[2]
+##                    Tcounter=0                   
+##                Ccounter= Ccounter+1
+##            elif row[0] not in activities:
+##                Ccounter=0
+##                Tcounter= Tcounter+1            
+
+    print(second, secondStop)
+    return[[first, firstStart, firstStop],[second, secondStart, secondStop]] 
